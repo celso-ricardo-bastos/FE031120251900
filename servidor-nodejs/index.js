12727e76cb7a72328dfce0739e68d23e1099b5ca
Celso <celsorico@gmail.comcd mnt/c/git config --global user.email celsorico@gmail.com>
2025-11-21 19:16:22 -0300

    servidor node

---
+++ servidor-nodejs/index.js
@@ -0,0 +1,37 @@
+let express = require('express');
+let cors = require('cors')
+const fs = require('fs')
+let app = express();
+
+const host = 'localhost';
+const port = '3001';
+
+// http://localhost:3001/hello
+
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(express.json())
+
+app.get('/hello', (request, response) => {
+    // response.send('<h1> Hello World!</h1>');
+});
+
+app.get('/cliente', (request, response) => {
+    // response.send('<h1> Hello World!</h1>');
+    console.log(request.query)
+    fs.writeFileSync('teste.json', JSON.stringify(request.query))
+
+    response.redirect('http://localhost:5500?sucesso');
+});
+
+app.post('/cliente', (request, response) => {
+    console.log("Dados: ", request.body)
+    fs.writeFileSync('teste.json', JSON.stringify(request.query))
+    response.json({status: 'sucesso'})
+
+    // response.redirect('http://localhost:5500');
+});
+
+app.listen(port, host, function() {
+    console.log(`Servidor em execução http://${host}:${port}`)
+});
